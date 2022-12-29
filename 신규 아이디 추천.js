1af0587721695560d9647bb8234fc17909b269a7
github wns0901 <github wnswns012@gmail.com>
2022-12-29 17:58:48 +0900

    신규 아이디 추천 진행중

---
+++ 신규 아이디 추천.js
@@ -0,0 +1,18 @@
+new_id = [
+  "...!@BaT#*..y.abcdefghijklm",
+  "z-+.^.",
+  "=.=",
+  "123_.def",
+  "abcdefghijklmn.p",
+];
+function solution(new_id) {
+  var answer = "";
+  id = new_id.toLowerCase();
+  let reg = /[^a-z0-9-_.]/gm;
+  id = id.replace(reg, "");
+  reg = /[.]+/gm;
+  id = id.replace(reg, ".");
+
+  return id;
+}
+console.log(solution(new_id[0]));
