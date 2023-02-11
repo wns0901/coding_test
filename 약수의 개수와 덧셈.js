faf34d532550511c443774c07300b6d13060c58a
wns0901 <wnswns012@gmail.com>
2023-02-11 17:16:23 +0900

    부족한 금액 계산하기, 약수의 개수와 덧셈

--- 약수의 개수와 덧셈.js
+++ 약수의 개수와 덧셈.js
@@ -9,7 +9,20 @@
 
 function solution(left, right) {
   var answer = 0;
+  for (let i = left; i <= right; i++) {
+    let count = 0;
+    for (let z = 1; z <= i; z++) {
+      if (i % z === 0) {
+        count++;
+      }
+    }
+    if (count % 2 === 0) {
+      answer += i;
+    } else {
+      answer -= i;
+    }
+  }
   return answer;
 }
 
-console.log(solution(13, 17));
+console.log(solution(24, 27));
